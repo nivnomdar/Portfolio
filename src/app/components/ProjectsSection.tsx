937f13c3d032cf0c3ad4e8219180c89b2ac4298a
niv nomdar <nivnomdar1@gmail.com>
2024-04-03 19:16:30 +0300

    Add project tabs

--- src/app/components/ProjectsSection.tsx
+++ src/app/components/ProjectsSection.tsx
@@ -1,5 +1,8 @@
-import React from "react";
+"use client";
+
+import React, { useState } from "react";
 import ProjectCard from "./ProjectCard";
+import ProjectTag from "./ProjectTag";
 
 const projectsData = [
   {
@@ -8,6 +11,7 @@ const projectsData = [
     description:
       "Create convertion, image, video, code and music using ai tools.",
     image: "/images/projects/Dashboard-Nif-ai.png",
+    tag: ["All", "Next.js", "Vercel"],
     gitUrl: "https://github.com/nivnomdar/Nif-ai",
   },
   {
@@ -15,6 +19,7 @@ const projectsData = [
     title: "Spotify Web",
     description: "Listen to music and choose your favorite songs.",
     image: "/images/projects/Dashboard-Nif-Spotify.png",
+    tag: ["All", "Next.js", "Vercel"],
     gitUrl: "https://github.com/nivnomdar/Nif-Spotify",
   },
   {
@@ -23,6 +28,7 @@ const projectsData = [
     description:
       "Search for movies or tvshows, see their information and add to your favorites.",
     image: "/images/projects/Dashboard-MoviesTvShows.png",
+    tag: ["All", "React", "Vercel"],
     gitUrl: "https://github.com/nivnomdar/Nif-MoviesTvShows-App",
   },
   {
@@ -31,6 +37,7 @@ const projectsData = [
     description:
       "Search Videogames, see their information and add to your favorites.",
     image: "/images/projects/Dashboard-VideoGames.png",
+    tag: ["All", "React", "Vercel"],
     gitUrl: "https://github.com/nivnomdar/Nif-GamesWeb",
   },
   {
@@ -38,6 +45,7 @@ const projectsData = [
     title: "Add-Note Project Web",
     description: "Add a new note, edit or remove it.",
     image: "/images/projects/Dashboard-Note.png",
+    tag: ["All", "html-css-js"],
     gitUrl: "https://github.com/nivnomdar/Note-Project-1",
   },
   {
@@ -46,6 +54,7 @@ const projectsData = [
     description:
       "Stay informed about cryptocurrency industry updates and market trends.",
     image: "/images/projects/Dashboard-Crypto.png",
+    tag: ["All", "html-css-js"],
     gitUrl: "https://github.com/nivnomdar/Crypto-Web-Project",
   },
   {
@@ -53,6 +62,7 @@ const projectsData = [
     title: "Meeting-Room Schedule Web",
     description: "Choose meeting room, team and time and add to the schedule.",
     image: "/images/projects/Dashoboard-MeetingRoomsSchedule.png",
+    tag: ["All", "React"],
     gitUrl: "https://github.com/nivnomdar/Meeting-Rooms-Schedule",
   },
   {
@@ -60,19 +70,57 @@ const projectsData = [
     title: "Cellphone Lab Web",
     description:
       "Improve customer service by managing waiting lists based on inquiry types. Collect and store phone numbers for prioritized assistance.",
+    tag: ["All", "html-css-js"],
     image: "/images/projects/Dashboard-CellphoneLab.png",
     gitUrl: "https://github.com/nivnomdar/CellKlum-Phone-Services",
   },
 ];
 
 const ProjectsSection = () => {
+  const [tag, setTag] = useState("All");
+
+  const handleTagChange = (newTag: string) => {
+    setTag(newTag);
+  };
+
+  const filteredProjects = projectsData.filter((project) =>
+    project.tag.includes(tag)
+  );
+
   return (
     <section id="projectsSection">
       <h2 className="text-center text-4xl font-bold text-white mt-4 mb-8 md:mb-12">
         My Projects
       </h2>
+      <div className="text-white flex flex-row justify-center items-start gap-2 py-6">
+        <ProjectTag
+          onClick={handleTagChange}
+          name="All"
+          isSelected={tag === "All"}
+        />
+        <ProjectTag
+          onClick={handleTagChange}
+          name="React"
+          isSelected={tag === "React"}
+        />
+        <ProjectTag
+          onClick={handleTagChange}
+          name="Next.js"
+          isSelected={tag === "Next.js"}
+        />
+        <ProjectTag
+          onClick={handleTagChange}
+          name="html-css-js"
+          isSelected={tag === "html-css-js"}
+        />
+        <ProjectTag
+          onClick={handleTagChange}
+          name="Vercel"
+          isSelected={tag === "Vercel"}
+        />
+      </div>
       <div className="grid md:grid-cols-2 gap-8 md:gap-12 mx-5">
-        {projectsData.map((project) => (
+        {filteredProjects.map((project) => (
           <ProjectCard
             key={project.id}
             title={project.title}
